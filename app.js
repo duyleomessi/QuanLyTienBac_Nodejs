9a5496ddfd2cec6313195f466d16d2b7e9fcfd7b
duyleomessi <duyleitbka95@gmail.com>
2018-08-06 10:39:42 +0700

    add x-access-token

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var db = require('./db');
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token');
   next();
 })
 
