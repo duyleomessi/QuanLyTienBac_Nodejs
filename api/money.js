a828b459c8a73e5ca9d46dbecf783544a235c1e5
duyleomessi <duyleitbka95@gmail.com>
2018-10-02 02:48:34 +0700

    add get monthly activities

--- api/money.js
+++ api/money.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var moneyApi = express.Router();
 var bodyParser = require('body-parser');
-moneyApi.use(bodyParser.urlencoded({ extended: false }));
+moneyApi.use(bodyParser.urlencoded({
+    extended: false
+}));
 moneyApi.use(bodyParser.json());
 
 var Money = require('../models/Money');
@@ -20,24 +22,59 @@ moneyApi.post('/', verifyToken, (req, res, next) => {
     newActivity.save((err) => {
         if (err) {
             console.log(err);
-            return res.status(500).json({message: err});
-        } 
+            return res.status(500).json({
+                message: err
+            });
+        }
 
-        return res.status(201).json({ message: "Done" });
+        return res.status(201).json({
+            message: "Done"
+        });
     })
 })
 
 // get all activity
 moneyApi.get('/', verifyToken, (req, res, next) => {
     Money.find({})
-        .sort({year: -1, month: -1, day: -1, updated: -1})
+        .sort({
+            year: -1,
+            month: -1,
+            day: -1,
+            updated: -1
+        })
         .exec((err, activities) => {
             if (err) {
                 console.log(err);
-                return res.status(500).json({message: err});
+                return res.status(500).json({
+                    message: err
+                });
             }
 
-            return res.status(200).json(activities); 
+            return res.status(200).json(activities);
+        })
+})
+
+moneyApi.get('/month/:id', verifyToken, (req, res, next) => {
+    const month = req.params.id;
+    // console.log(month);
+    Money.find({
+            month: month
+        })
+        .sort({
+            year: -1,
+            month: -1,
+            day: -1,
+            updated: -1
+        })
+        .exec((err, activities) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    message: err
+                });
+            }
+
+            return res.status(200).json(activities);
         })
 })
 
